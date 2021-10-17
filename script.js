a0d05519e65cb7527aafb5c99c6bd914da24404c
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-10-17 00:34:02 -0400

    refactoring

--- script.js
+++ script.js
@@ -3,7 +3,25 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
-for(let i = 0; i < btnsOpenModal.length; i++){
-  console.log(btnsOpenModal[i].textContent);
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 }
+
+for (let i = 0; i < btnsOpenModal.length; i++) btnsOpenModal[i].addEventListener('click', openModal)
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// overlay.addEventListener('click', function(){
+//   closeModal();
+// })
+// btnCloseModal.addEventListener('click', function(){
+//   closeModal();
+// })
